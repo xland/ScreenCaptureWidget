67246f295b48376dbdff822b171a2da5ddbf3302
xland <412588801@qq.com>
2025-02-10 12:38:22 +0800

    ready

--- Qt5ScreenCapture2/WindowMain.cpp
+++ Qt5ScreenCapture2/WindowMain.cpp
@@ -9,28 +9,58 @@
 #include <qdebug.h>
 #include "WindowMain.h"
 
-WindowMain::WindowMain(QWidget* parent) : QWidget(parent)
+WindowMain::WindowMain() : QWindow()
 {
-    this->setWindowFlags(Qt::FramelessWindowHint);
+    setSurfaceType(QWindow::RasterSurface);
+    setFlags(Qt::FramelessWindowHint | Qt::Window);
     x = GetSystemMetrics(SM_XVIRTUALSCREEN);
     y = GetSystemMetrics(SM_YVIRTUALSCREEN);
     w = GetSystemMetrics(SM_CXVIRTUALSCREEN);
     h = GetSystemMetrics(SM_CYVIRTUALSCREEN);
     printScreen();
     //注意 必须先设置大小并显示窗口后，再使用原生API重置大小
-    setFixedSize(w, h);
+    setMaximumSize(QSize(w, h));
+    setMinimumSize(QSize(w, h));
     show();
     //注意 必须先重置大小，再设置缩放比例
     auto hwnd = (HWND)winId();
     SetWindowPos(hwnd, nullptr, x, y, w, h, SWP_NOZORDER | SWP_SHOWWINDOW);
     //注意 这里必须用窗口的dpr来设置img的dpr，不能用主屏的dpr，此操作必须最后执行
-    auto dpr = windowHandle()->devicePixelRatio();
+    auto dpr = devicePixelRatio();
     img.setDevicePixelRatio(dpr);
+
+    backingStore = new QBackingStore(this);
+    backingStore->resize(QSize(w,h));
 }
 
 WindowMain::~WindowMain()
 {
     
+}
+
+void WindowMain::renderNow()
+{
+    if (!isExposed()) return;
+    QRect rect(0, 0, w, h);
+    backingStore->beginPaint(rect);
+
+    QPaintDevice* device = backingStore->paintDevice();
+    QPainter painter(device);
+    painter.drawImage(QPoint(0, 0), img);
+    draw100Rect(painter);
+    painter.setBrush(QColor(0, 0, 0, 120));
+    QPainterPath path;
+    path.addRect(-1, -1, w + 1, h + 1);
+    path.addRect(rectMask);
+    painter.drawPath(path);
+    QColor borderColor(22, 118, 255);
+    painter.setPen(QPen(QBrush(borderColor), 2));
+    painter.setBrush(Qt::NoBrush);
+    painter.drawRect(rectMask);
+
+    painter.end();
+    backingStore->endPaint();
+    backingStore->flush(rect);
 }
 
 void WindowMain::printScreen()
@@ -62,22 +92,29 @@ void WindowMain::draw100Rect(QPainter& painter)
     qDebug() << "draw100Rect:" << timer.elapsed() << "ms";
 }
 
-void WindowMain::paintEvent(QPaintEvent* event)
-{
-    QWidget::paintEvent(event);
-    QPainter painter(this);
-    painter.drawImage(QPoint(0, 0), img);
+//void WindowMain::paintEvent(QPaintEvent* event)
+//{
+//    QWidget::paintEvent(event);
+//    QPainter painter(this);
+//    painter.drawImage(QPoint(0, 0), img);
+//
+//    draw100Rect(painter);
+//    painter.setBrush(QColor(0, 0, 0, 120));
+//    QPainterPath path;
+//    path.addRect(-1, -1, w + 1, h + 1);
+//    path.addRect(rectMask);
+//    painter.drawPath(path);
+//    QColor borderColor(22, 118, 255);
+//    painter.setPen(QPen(QBrush(borderColor), 2));
+//    painter.setBrush(Qt::NoBrush);
+//    painter.drawRect(rectMask);
+//}
 
-    draw100Rect(painter);
-    painter.setBrush(QColor(0, 0, 0, 120));
-    QPainterPath path;
-    path.addRect(-1, -1, w + 1, h + 1);
-    path.addRect(rectMask);
-    painter.drawPath(path);
-    QColor borderColor(22, 118, 255);
-    painter.setPen(QPen(QBrush(borderColor), 2));
-    painter.setBrush(Qt::NoBrush);
-    painter.drawRect(rectMask);
+void WindowMain::exposeEvent(QExposeEvent*)
+{
+    if (isExposed()) {
+        renderNow();
+    }
 }
 
 void WindowMain::mousePressEvent(QMouseEvent* event)
@@ -98,6 +135,7 @@ void WindowMain::mouseMoveEvent(QMouseEvent* event)
         auto pos = event->pos();
         rectMask.setCoords(posPress.x(), posPress.y(), pos.x(), pos.y());
         rectMask = rectMask.normalized();
-        update();
+        //update();
+        renderNow();
     }
 }
--- Qt5ScreenCapture2/WindowMain.h
+++ Qt5ScreenCapture2/WindowMain.h
@@ -1,22 +1,25 @@
 #pragma once
 #include <Windows.h>
-#include <QWidget>
+#include <QWindow>
 #include <QImage>
+#include <QPainter>
+#include <QBackingStore>
 
-class WindowMain  : public QWidget
+class WindowMain  : public QWindow
 {
 	Q_OBJECT
 
 public:
-	WindowMain(QWidget* parent = nullptr);
+	WindowMain();
 	~WindowMain();
 protected:
-	void paintEvent(QPaintEvent* event) override;
+	//void paintEvent(QPaintEvent* event) override;
+	void exposeEvent(QExposeEvent*) override;
 	void mousePressEvent(QMouseEvent* event) override;
 	void mouseReleaseEvent(QMouseEvent* event) override;
 	void mouseMoveEvent(QMouseEvent* event) override;
-
 private:
+	void renderNow();
 	void printScreen();
 	void draw100Rect(QPainter& painter);
 	int x, y, w, h;
@@ -24,4 +27,5 @@ private:
 	bool isPressed = false;
 	QPoint posPress;
 	QRect rectMask;
+	QBackingStore* backingStore;
 };
